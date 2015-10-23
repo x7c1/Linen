7dd0f8647bc2662180669a1791b87b121a8a88ed
x7c1 <jmtaro+x7c1@gmail.com>
2015-10-23 18:01:23 +0900

    move concrete class to private

--- starter/src/main/java/x7c1/linen/CommentRowInspector.java
+++ starter/src/main/java/x7c1/linen/CommentRowInspector.java
@@ -32,4 +32,32 @@ public class CommentRowInspector implements ViewInspector<CommentRowHolder> {
 			(TextView) layout.findViewById(R.id.comment_row_content)
 		);
 	}
+	private static class CommentRowHolderImpl implements CommentRowHolder {
+
+		private final View layout;
+		private final TextView name;
+		private final TextView content;
+
+		public CommentRowHolderImpl(View layout, TextView name, TextView content) {
+			this.layout = layout;
+			this.name = name;
+			this.content = content;
+		}
+
+		@Override
+		public TextView name() {
+			return name;
+		}
+
+		@Override
+		public TextView content() {
+			return content;
+		}
+
+		@Override
+		public View layout() {
+			return layout;
+		}
+
+	}
 }
