02b31530b880cce9dc7fed80489d080ee2040343
x7c1 <jmtaro+x7c1@gmail.com>
2015-11-11 23:08:17 +0900

    add RichRecyclerView

---
+++ wheat-modern/src/main/scala/x7c1/wheat/modern/decorator/RichRecyclerView.scala
@@ -0,0 +1,24 @@
+package x7c1.wheat.modern.decorator
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.OnScrollListener
+
+class RichRecyclerView[A <: RecyclerView](view: A){
+  def onScroll[B](f: ScrollEvent[A] => B): Unit = {
+    view addOnScrollListener new OnScroll(f)
+  }
+}
+
+private class OnScroll[A <: RecyclerView, B](
+  f: ScrollEvent[A] => B) extends OnScrollListener {
+
+  override def onScrolled(view: RecyclerView, x: Int, y: Int): Unit = {
+    val event = new ScrollEvent[A]{
+      override def targetView: A = view.asInstanceOf[A]
+      override def dx: Int = x
+      override def dy: Int = y
+    }
+    f(event)
+  }
+
+}
