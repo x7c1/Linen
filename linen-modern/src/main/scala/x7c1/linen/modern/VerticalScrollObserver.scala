7c68ad10ddcd3edbb659181468d6e73d188c9a85
x7c1 <jmtaro+x7c1@gmail.com>
2015-11-24 20:03:25 +0900

    extract polling procedure to reuse

--- linen-modern/src/main/scala/x7c1/linen/modern/VerticalScrollObserver.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/VerticalScrollObserver.scala
@@ -1,39 +1,19 @@
 package x7c1.linen.modern
 
-import java.util.{Timer, TimerTask}
-
 import android.support.v7.widget.RecyclerView
+import x7c1.wheat.modern.chrono.BufferingChangeWatcher
 
 class VerticalScrollObserver(
   view: RecyclerView,
-  onScrollStopped: OnScrollStoppedListener ) {
+  listener: OnScrollStoppedListener ) {
 
-  private var position: Option[Int] = None
-
-  private val timer = new Timer()
-
-  private var task: Option[TimerTask] = None
-
-  def touch(): Unit = {
-    task.foreach{_.cancel()}
-    task = Some apply newTask
-    task foreach { timer.schedule(_, 100, 100) }
-  }
-
-  def cancel(): Unit = timer.cancel()
-
-  private def newTask = new TimerTask {
-    override def run(): Unit = {
-      val current = view.computeVerticalScrollOffset()
-      position match {
-        case Some(previous) if previous == current =>
-          cancel()
-          onScrollStopped.onScrollStopped(new ScrollStoppedEvent(current))
-        case _ =>
-      }
-      position = Some(current)
+  private val watcher = new BufferingChangeWatcher[Int](
+    getCurrent = () => view.computeVerticalScrollOffset(),
+    onStop = current => {
+      listener onScrollStopped new ScrollStoppedEvent(current)
     }
-  }
+  )
+  def touch(): Unit = watcher.touch()
 }
 
 trait OnScrollStoppedListener {
