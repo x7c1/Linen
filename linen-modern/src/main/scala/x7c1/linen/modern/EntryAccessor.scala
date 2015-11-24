8f691bc9551db926f184b1a73be6fb9acaadd55c
x7c1 <jmtaro+x7c1@gmail.com>
2015-11-24 12:32:01 +0900

    move class to another file

--- linen-modern/src/main/scala/x7c1/linen/modern/EntryAccessor.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/EntryAccessor.scala
@@ -99,31 +99,6 @@ class EntryLoadExecutor(cacher: EntryCacher){
   }
 }
 
-sealed trait EntryLoaderEvent
-
-case class EntryLoadedEvent(
-  sourceId: Long,
-  entries: Seq[Entry] ) extends EntryLoaderEvent
-
-trait OnEntryLoadedListener { self =>
-  def onEntryLoaded(e: EntryLoadedEvent): Unit
-
-  def append(listener: OnEntryLoadedListener): OnEntryLoadedListener = {
-    new OnEntryLoadedListener {
-      override def onEntryLoaded(e: EntryLoadedEvent): Unit = {
-        self onEntryLoaded e
-        listener onEntryLoaded e
-      }
-    }
-  }
-}
-object OnEntryLoadedListener {
-  def apply(f: EntryLoadedEvent => Unit): OnEntryLoadedListener =
-    new OnEntryLoadedListener {
-      override def onEntryLoaded(e: EntryLoadedEvent): Unit = f(e)
-    }
-}
-
 class SourceStateUpdater(
   sourceStateBuffer: SourceStateBuffer) extends OnEntryLoadedListener {
 
