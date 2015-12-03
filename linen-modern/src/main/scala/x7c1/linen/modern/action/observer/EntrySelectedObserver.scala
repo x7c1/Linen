8f158fc1a081707a0c8066d6a649b0cfb0a0f5e9
x7c1 <jmtaro+x7c1@gmail.com>
2015-12-03 14:53:10 +0900

    improve scrolling  performance
    when selecting a entry

--- linen-modern/src/main/scala/x7c1/linen/modern/action/observer/EntrySelectedObserver.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/action/observer/EntrySelectedObserver.scala
@@ -9,16 +9,13 @@ class EntrySelectedObserver(actions: Actions) extends OnEntrySelectedListener {
   override def onEntrySelected(event: EntrySelectedEvent): Unit = {
     Log info s"[init] ${event.dump}"
 
-    val x1 = for {
+    val async = for {
+      _ <- actions.detailArea onEntrySelected event
       _ <- actions.entryArea onEntrySelected event
       _ <- actions.container onEntrySelected event
-    } yield ()
-
-    val x2 = for {
       _ <- actions.sourceArea onEntrySelected event
-      _ <- actions.detailArea onEntrySelected event
     } yield ()
 
-    Seq(x1, x2) foreach runAsync { Log error _.toString }
+    Seq(async) foreach runAsync { Log error _.toString }
   }
 }
