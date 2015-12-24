d06682242f1fdcc0bf711605f0ed81776ba8ee58
x7c1 <jmtaro+x7c1@gmail.com>
2015-12-24 16:19:32 +0900

    shorten time to build

--- project/LinenBuild.scala
+++ project/LinenBuild.scala
@@ -21,6 +21,7 @@ object LinenBuild extends Build with LinenSettings {
   )
   lazy val testLibrary = "org.scalatest" %% "scalatest" % "2.2.4" % Test
   lazy val scalaz = "org.scalaz" %% "scalaz-concurrent" % "7.1.5"
+  lazy val akka = "com.typesafe.akka" %% "akka-actor" % "2.3.14"
 
   lazy val `wheat-ancient` = project.
     settings(linenSettings:_*).
@@ -34,6 +35,7 @@ object LinenBuild extends Build with LinenSettings {
   lazy val `linen-pickle` = project.
     settings(linenSettings:_*).
     settings(libraryDependencies += scalaz).
+    settings(libraryDependencies += akka).
     settings(
       unmanagedJars in Compile := androidSdkClasspath,
       assemblyOutputPath in assembly := pickleJarPath.value,
@@ -54,7 +56,7 @@ object LinenBuild extends Build with LinenSettings {
   lazy val `linen-modern` = project.
     settings(linenSettings:_*).
     settings(libraryDependencies += scalaz).
-    settings(libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.14").
+    settings(libraryDependencies += akka).
     settings(libraryDependencies ++= Seq(
       "com.novocode" % "junit-interface" % "0.11" % Test,
       "org.apache.maven" % "maven-ant-tasks" % "2.1.3" % Test,
@@ -141,6 +143,9 @@ trait LinenSettings {
 
   lazy val discardTargets: Def.Initialize[String => MergeStrategy] = {
     val ignore = (path: String) =>
+      (path startsWith "akka") ||
+      (path == "reference.conf") ||
+      (path startsWith "com/typesafe") ||
       (path startsWith "scalaz") ||
       (path startsWith "x7c1/linen/glue") ||
       (path startsWith "x7c1/wheat/ancient")
@@ -148,6 +153,9 @@ trait LinenSettings {
     Def.setting {
       case path if ignore(path) => MergeStrategy.discard
       case path =>
+        if (!(path startsWith "x7c1")){
+          sLog.value info s"[$path] may be duplicate between linen-pickle and linen-modern"
+        }
         val original = (assemblyMergeStrategy in assembly).value
         original(path)
     }
