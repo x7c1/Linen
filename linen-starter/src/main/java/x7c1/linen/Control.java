8b4c8adc4b35fdfce39d5ee20748fd37d1cd9e67
x7c1 <jmtaro+x7c1@gmail.com>
2016-01-15 03:22:53 +0900

    remove unused methods

--- linen-starter/src/main/java/x7c1/linen/Control.java
+++ linen-starter/src/main/java/x7c1/linen/Control.java
@@ -1,8 +1,5 @@
 package x7c1.linen;
 
-import android.content.Context;
-import android.content.Intent;
-
 import x7c1.linen.dev.CreateRecordsActivity;
 import x7c1.linen.glue.activity.ActivityLabel;
 import x7c1.linen.glue.service.ServiceLabel;
@@ -26,21 +23,6 @@ public class Control {
 		return klass;
 	}
 
-	public static Intent getActivityIntentOf(Context context, ActivityLabel label) {
-		final Intent intent;
-		switch (label){
-			case SettingChannels:
-				intent = new Intent(context, SettingChannelsActivity.class);
-				break;
-			case CreateRecords:
-				intent = new Intent(context, CreateRecordsActivity.class);
-				break;
-			default:
-				throw new IllegalArgumentException(format("unknown activity label: %s", label));
-		}
-		return intent;
-	}
-
 	public static Class<?> getServiceClassOf(ServiceLabel label) {
 		final Class<?> klass;
 		switch(label){
@@ -51,17 +33,5 @@ public class Control {
 				throw new IllegalArgumentException(format("unknown service label: %s", label));
 		}
 		return klass;
-	}
-
-	public static Intent getServiceIntentOf(Context context, ServiceLabel label) {
-		final Intent intent;
-		switch(label){
-			case Updater:
-				intent = new Intent(context, UpdaterService.class);
-				break;
-			default:
-				throw new IllegalArgumentException(format("unknown service label: %s", label));
-		}
-		return intent;
 	}
 }
