8f2e4808c6a2ffa7e8fa4952589d474074c912d7
x7c1 <jmtaro+x7c1@gmail.com>
2016-01-14 23:02:41 +0900

    add example of InboxStyle

--- linen-modern/src/main/scala/x7c1/linen/modern/init/updater/UpdaterServiceDelegatee.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/updater/UpdaterServiceDelegatee.scala
@@ -1,12 +1,11 @@
 package x7c1.linen.modern.init.updater
 
 import android.app.Notification.Builder
-import android.app.{NotificationManager, PendingIntent, Service}
+import android.app.{Notification, PendingIntent, NotificationManager, Service}
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.os.IBinder
-import x7c1.linen.glue.service.ServiceControl
-import x7c1.linen.glue.service.ServiceLabel.Updater
+import x7c1.linen.glue.service.{ServiceLabel, ServiceControl}
 import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.modern.decorator.service.CommandStartType
 import x7c1.wheat.modern.decorator.service.CommandStartType.NotSticky
@@ -17,21 +16,37 @@ class UpdaterServiceDelegatee(service: Service with ServiceControl){
     Log info "[init]"
     None
   }
+
+  var notificationId: Int = 1
+
   def onStartCommand(intent: Intent, flags: Int, startId: Int): CommandStartType = {
     Log info "[init]"
 
-    val notificationIntent = new Intent(service, service getClassOf Updater)
-    val pendingIntent = PendingIntent.getService(service, 0, notificationIntent, 0)
+    val notificationIntent = new Intent(service, service getClassOf ServiceLabel.Updater)
+    val pendingIntent = PendingIntent.getService(service, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
     val builder = new Builder(service).
       setContentIntent(pendingIntent).
-      setContentTitle("sample linen title").
-      setContentText("sample linen test").
+      setContentTitle(s"sample title $notificationId").
+      setContentText(s"sample text $notificationId").
+      setProgress(100, notificationId, false).
       setSmallIcon(android.R.drawable.ic_dialog_info)
 
-    val notification = builder.build()
+    val style0 = new Notification.InboxStyle(builder)
+      .setSummaryText(s"summary $notificationId")
+      .setBigContentTitle(s"title $notificationId")
+
+    val style = (1 to notificationId).foldLeft(style0){
+      case (s, i) => s.addLine(s"line $i")
+    }
+    val notification = style.build()
+
     val manager = service.getSystemService(NOTIFICATION_SERVICE).asInstanceOf[NotificationManager]
-    manager.notify(123, notification)
-    service.startForeground(123, notification)
+
+    Log info s"[n-id] $notificationId"
+    service.startForeground(notificationId, notification)
+    manager.notify(notificationId, notification)
+
+    notificationId += 1
 
     NotSticky
   }
