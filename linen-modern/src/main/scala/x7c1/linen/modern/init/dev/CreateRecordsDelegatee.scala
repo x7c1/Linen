d7327e1ec8d1d09d1e6c55421e3e0a8d3ac0e27e
x7c1 <jmtaro+x7c1@gmail.com>
2016-01-15 03:18:48 +0900

    add example of bound service

--- linen-modern/src/main/scala/x7c1/linen/modern/init/dev/CreateRecordsDelegatee.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/dev/CreateRecordsDelegatee.scala
@@ -1,29 +1,64 @@
 package x7c1.linen.modern.init.dev
 
 import android.app.Activity
-import android.content.Intent
+import android.content.Context.BIND_AUTO_CREATE
+import android.content.{ComponentName, Intent, ServiceConnection}
+import android.os.{Handler, Message, RemoteException, Messenger, IBinder}
 import android.widget.Toast
 import x7c1.linen.glue.res.layout.DevCreateRecordsLayout
 import x7c1.linen.glue.service.ServiceControl
 import x7c1.linen.glue.service.ServiceLabel.Updater
 import x7c1.linen.modern.accessor.LinenDatabase
+import x7c1.linen.modern.init.dev.CreateRecordsDelegatee.{MessageTypeForUnregister, MessageTypeForSet, MessageTypeForRegister}
 import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.modern.decorator.Imports._
 import x7c1.wheat.modern.patch.TaskAsync.async
 
+object CreateRecordsDelegatee {
+  val MessageTypeForRegister = 1
+  val MessageTypeForSet = 2
+  val MessageTypeForProgress = 3
+  val MessageTypeForUnregister = 3
+}
 
 class CreateRecordsDelegatee (
   activity: Activity with ServiceControl,
   layout: DevCreateRecordsLayout){
 
+  private var serviceMessenger: Option[Messenger] = None
+
+  private val handlerMessenger = new Messenger(new IncomingHandler)
+
+  lazy val connection = new ServiceConnection {
+    override def onServiceConnected(name: ComponentName, service: IBinder): Unit = {
+      serviceMessenger = Some apply new Messenger(service)
+      try {
+        val message = Message.obtain(null, MessageTypeForRegister)
+        message.replyTo = handlerMessenger
+        serviceMessenger foreach (_ send message)
+
+        val message2 = Message.obtain(null, MessageTypeForSet, this.hashCode(), 0)
+        serviceMessenger foreach (_ send message2)
+      } catch {
+        case e: RemoteException => Log error e.getMessage
+      }
+      Log info "[done]"
+    }
+    override def onServiceDisconnected(name: ComponentName): Unit = {
+      Log info "[done]"
+    }
+  }
   def setup(): Unit = {
+    val intent = new Intent(activity, activity getClassOf Updater)
+    activity.bindService(intent, connection, BIND_AUTO_CREATE)
+
     layout.toolbar onClickNavigation { _ =>
       activity.finish()
     }
     layout.createDummies onClick { _ =>
       async {
-        activity startService new Intent(
-          activity, activity getClassOf Updater )
+        Log info "start"
+        activity.startService(intent)
       }
     }
     layout.deleteDatabase onClick { _ =>
@@ -34,5 +69,27 @@ class CreateRecordsDelegatee (
 
   def close(): Unit = {
     Log info "[done]"
+
+    serviceMessenger foreach { messenger =>
+      try {
+        val message = Message.obtain(null, MessageTypeForUnregister)
+        message.replyTo = handlerMessenger
+        messenger send message
+      } catch {
+        case e: RemoteException => Log error e.getMessage
+      }
+    }
+    activity unbindService connection
+  }
+}
+
+class IncomingHandler extends Handler {
+  override def handleMessage(msg: Message): Unit = {
+    msg.what match {
+      case MessageTypeForSet =>
+        Log info s"received sample ${msg.arg1} $msg"
+      case _ =>
+        Log info s"received other $msg, ${msg.getData}"
+    }
   }
 }
--- linen-modern/src/main/scala/x7c1/linen/modern/init/updater/UpdaterServiceDelegatee.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/updater/UpdaterServiceDelegatee.scala
@@ -1,26 +1,32 @@
 package x7c1.linen.modern.init.updater
 
 import android.app.Notification.Builder
-import android.app.{Notification, PendingIntent, NotificationManager, Service}
+import android.app.{Notification, NotificationManager, PendingIntent, Service}
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
-import android.os.IBinder
-import x7c1.linen.glue.service.{ServiceLabel, ServiceControl}
+import android.os.{Bundle, Handler, IBinder, Message, Messenger, RemoteException}
+import x7c1.linen.glue.service.{ServiceControl, ServiceLabel}
+import x7c1.linen.modern.init.dev.CreateRecordsDelegatee.{MessageTypeForProgress, MessageTypeForRegister, MessageTypeForSet, MessageTypeForUnregister}
 import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.modern.decorator.service.CommandStartType
 import x7c1.wheat.modern.decorator.service.CommandStartType.NotSticky
 
+import scala.collection.mutable.ListBuffer
+
 class UpdaterServiceDelegatee(service: Service with ServiceControl){
+
+  private val clients = ListBuffer[Messenger]()
+  private val messenger = new Messenger(new IncomingHandler2(clients))
 
   def onBind(intent: Intent): Option[IBinder] = {
     Log info "[init]"
-    None
+    Some(messenger.getBinder)
   }
 
   var notificationId: Int = 1
 
   def onStartCommand(intent: Intent, flags: Int, startId: Int): CommandStartType = {
-    Log info "[init]"
+    Log info s"[init] start:$startId"
 
     val notificationIntent = new Intent(service, service getClassOf ServiceLabel.Updater)
     val pendingIntent = PendingIntent.getService(service, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
@@ -46,11 +52,48 @@ class UpdaterServiceDelegatee(service: Service with ServiceControl){
     service.startForeground(notificationId, notification)
     manager.notify(notificationId, notification)
 
+    clients foreach { client =>
+      val message = Message.obtain(null, MessageTypeForProgress)
+      message setData {
+        val bundle = new Bundle()
+        bundle.putString("hoge-", s"hoge-id:$notificationId")
+        bundle
+      }
+      client.send(message)
+    }
     notificationId += 1
 
     NotSticky
   }
   def onDestroy(): Unit = {
     Log info "[init]"
+  }
+}
+
+class IncomingHandler2(clients: ListBuffer[Messenger]) extends Handler {
+
+  override def handleMessage(msg: Message): Unit = {
+    Log info s"[init] $msg"
+
+    msg.what match {
+      case MessageTypeForRegister =>
+        clients append msg.replyTo
+      case MessageTypeForSet =>
+        clients foreach { messenger =>
+          try {
+            messenger.send(Message.obtain(null, MessageTypeForSet, msg.arg1, 0))
+          } catch {
+            case e: RemoteException =>
+              Log error e.getMessage
+          }
+        }
+      case MessageTypeForUnregister =>
+        clients indexOf msg.replyTo match {
+          case i if i < 0 => Log error s"unknown message ${msg.replyTo}"
+          case i => clients remove i
+        }
+      case _ =>
+        super.handleMessage(msg)
+    }
   }
 }
