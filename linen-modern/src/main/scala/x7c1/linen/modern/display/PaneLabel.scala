67b1cad44f3532dd6324c272877289adf33454c1
x7c1 <jmtaro+x7c1@gmail.com>
2016-02-04 01:55:51 +0900

    use self-type

--- linen-modern/src/main/scala/x7c1/linen/modern/display/PaneLabel.scala
+++
@@ -1,9 +0,0 @@
-package x7c1.linen.modern.display
-
-sealed trait PaneLabel
-
-object PaneLabel {
-  case object SourceArea extends PaneLabel
-  case object EntryArea extends PaneLabel
-  case object EntryDetailArea extends PaneLabel
-}
