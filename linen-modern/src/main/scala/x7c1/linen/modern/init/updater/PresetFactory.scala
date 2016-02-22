78cff868ff92315df3c505272254c00c9f24ac67
x7c1 <jmtaro+x7c1@gmail.com>
2016-02-22 14:53:08 +0900

    enrich error message for preset factory

--- linen-modern/src/main/scala/x7c1/linen/modern/init/updater/PresetFactory.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/updater/PresetFactory.scala
@@ -2,6 +2,7 @@ package x7c1.linen.modern.init.updater
 
 import android.content.Context
 import x7c1.linen.modern.accessor.{ChannelSourceParts, ChannelOwner, ChannelAccessor, AccountAccessor, LinenOpenHelper}
+import x7c1.wheat.macros.logger.Log
 
 class PresetFactory (context: Context, helper: LinenOpenHelper){
   def createPreset() = {
@@ -12,23 +13,31 @@ class PresetFactory (context: Context, helper: LinenOpenHelper){
     } yield {
       new ChannelOwner(db, channelId, accountId)
     }
-    channelOwner addSource ChannelSourceParts(
-      url = "http://www.gizmodo.jp/atom.xml",
-      title = "ギズモード・ジャパン",
-      description = "ガジェット情報満載ブログ",
-      rating = 100
+    val partsList = Seq(
+      ChannelSourceParts(
+        url = "http://www.gizmodo.jp/atom.xml",
+        title = "ギズモード・ジャパン",
+        description = "ガジェット情報満載ブログ",
+        rating = 100
+      ),
+      ChannelSourceParts(
+        url = "http://feed.rssad.jp/rss/gigazine/rss_2.0",
+        title = "GIGAZINE",
+        description = "日々のあらゆるシーンで役立つ情報を提供するIT系ニュースサイト。毎日更新中。",
+        rating = 100
+      ),
+      ChannelSourceParts(
+        url = "http://wired.jp/feed/",
+        title = "WIRED.jp",
+        description = "未来をつくるIDEAS + INNOVATIONS。最新テクノロジーニュース、気になる人物インタビュー、ガジェット情報、サイエンスの最前線など、「未来のトレンド」を毎日発信。US/UK/ITALIA版WIREDからの翻訳記事をいち早くお届け。日本発、注目の動向を追ったオリジナル記事も満載。",
+        rating = 98
+      )
     )
-    channelOwner addSource ChannelSourceParts(
-      url = "http://feed.rssad.jp/rss/gigazine/rss_2.0",
-      title = "GIGAZINE",
-      description = "日々のあらゆるシーンで役立つ情報を提供するIT系ニュースサイト。毎日更新中。",
-      rating = 100
-    )
-    channelOwner addSource ChannelSourceParts(
-      url = "http://wired.jp/feed/",
-      title = "WIRED.jp",
-      description = "未来をつくるIDEAS + INNOVATIONS。最新テクノロジーニュース、気になる人物インタビュー、ガジェット情報、サイエンスの最前線など、「未来のトレンド」を毎日発信。US/UK/ITALIA版WIREDからの翻訳記事をいち早くお届け。日本発、注目の動向を追ったオリジナル記事も満載。",
-      rating = 98
-    )
+    partsList foreach { parts =>
+      channelOwner addSource parts match {
+        case Left(e) => Log error s"url:${parts.url} ${e.getMessage}"
+        case Right(b) => Log info s"inserted: ${parts.url}"
+      }
+    }
   }
 }
