fe7d72192a7c99e734b4c2f354c30b7a5c50e2f4
x7c1 <jmtaro+x7c1@gmail.com>
2016-03-19 11:08:50 +0900

    modify source_ratings to use TypedFields

--- linen-modern/src/main/scala/x7c1/linen/modern/accessor/database/source_ratings.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/accessor/database/source_ratings.scala
@@ -3,8 +3,14 @@ package x7c1.linen.modern.accessor.database
 import android.content.ContentValues
 import x7c1.linen.modern.accessor.Insertable
 import x7c1.linen.modern.struct.Date
+import x7c1.wheat.macros.database.TypedFields
 
-
+trait source_ratings extends TypedFields {
+  def source_id: Long
+  def account_id: Long
+  def rating: Int
+  def created_at: Int --> Date
+}
 
 case class SourceRatingParts(
   sourceId: Long,
@@ -17,12 +23,13 @@ object SourceRatingParts {
   implicit object insertable extends Insertable[SourceRatingParts] {
     override def tableName: String = "source_ratings"
     override def toContentValues(target: SourceRatingParts): ContentValues = {
-      val values = new ContentValues()
-      values.put("source_id", target.sourceId: java.lang.Long)
-      values.put("account_id", target.accountId: java.lang.Long)
-      values.put("rating", target.rating: java.lang.Integer)
-      values.put("created_at", target.createdAt.timestamp: java.lang.Integer)
-      values
+      val column = TypedFields.expose[source_ratings]
+      TypedFields toContentValues (
+        column.source_id -> target.sourceId,
+        column.account_id -> target.accountId,
+        column.rating -> target.rating,
+        column.created_at -> target.createdAt
+      )
     }
   }
 }
