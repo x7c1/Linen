c68bf6f67be2859f9962b4721677e95a3aa8da19
x7c1 <jmtaro+x7c1@gmail.com>
2016-03-23 23:56:16 +0900

    redraw more smoothly

--- linen-modern/src/main/scala/x7c1/linen/modern/init/unread/OnAccessorsLoadedListener.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/unread/OnAccessorsLoadedListener.scala
@@ -3,29 +3,36 @@ package x7c1.linen.modern.init.unread
 import x7c1.linen.glue.res.layout.MainLayout
 import x7c1.linen.modern.action.DrawerAction
 import x7c1.wheat.macros.logger.Log
-import x7c1.wheat.modern.decorator.Imports._
+import x7c1.wheat.modern.callback.CallbackTask.task
+import x7c1.wheat.modern.tasks.Async.await
+import x7c1.wheat.modern.tasks.UiThread
 
 
-class OnAccessorsLoadedListener(layout: MainLayout, drawer: DrawerAction){
+class OnAccessorsLoadedListener(layout: MainLayout, drawer: => DrawerAction){
   def onLoad(e: AccessorsLoadedEvent): Unit = {
     Log info s"[init] $e"
-    layout.itemView runUi { _ =>
 
-      /*
-      2015-12-20:
-      it should be written like:
-        layout.sourceList.getAdapter.notifyItemRangeInserted(0, ...)
+    (for {
+      ui <- task {
+        UiThread via layout.itemView
+      }
+      _ <- ui { _ => updateAdapter() }
+      _ <- await(50)
+      _ <- ui { _ => drawer.onBack() }
+    } yield ()).execute()
+  }
+  private def updateAdapter() = {
+    /*
+    2015-12-20:
+    it should be written like:
+      layout.sourceList.getAdapter.notifyItemRangeInserted(0, ...)
 
-      but this 'notifyItemRangeInserted' causes following error (and crash)
-        java.lang.IndexOutOfBoundsException:
-          Inconsistency detected. Invalid view holder adapter positionViewHolder
-      */
-
-      layout.sourceList.getAdapter.notifyDataSetChanged()
-      layout.entryList.getAdapter.notifyDataSetChanged()
-      layout.entryDetailList.getAdapter.notifyDataSetChanged()
-
-      drawer.onBack()
-    }
+    but this 'notifyItemRangeInserted' causes following error (and crash)
+      java.lang.IndexOutOfBoundsException:
+        Inconsistency detected. Invalid view holder adapter positionViewHolder
+    */
+    layout.sourceList.getAdapter.notifyDataSetChanged()
+    layout.entryList.getAdapter.notifyDataSetChanged()
+    layout.entryDetailList.getAdapter.notifyDataSetChanged()
   }
 }
