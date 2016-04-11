1c66a31a3134a842facf830899fc83c2db51f0c5
x7c1 <jmtaro+x7c1@gmail.com>
2016-04-11 23:55:07 +0900

    add menu to load source

--- linen-modern/src/main/scala/x7c1/linen/modern/init/settings/source/OnSourceMenuSelected.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/settings/source/OnSourceMenuSelected.scala
@@ -4,17 +4,22 @@ import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.PopupMenu
 import android.support.v7.widget.PopupMenu.OnMenuItemClickListener
-import android.view.{MenuItem, Menu}
-import x7c1.linen.glue.res.layout.{SettingSourceAttachRowItem, SettingSourceAttach}
+import android.view.{Menu, MenuItem}
+import x7c1.linen.glue.res.layout.{SettingSourceAttach, SettingSourceAttachRowItem}
+import x7c1.linen.glue.service.ServiceControl
+import x7c1.linen.glue.service.ServiceLabel.Updater
 import x7c1.linen.modern.display.settings.SourceMenuSelected
 import x7c1.linen.modern.init.settings.preset.AttachSourceDialog
+import x7c1.linen.modern.init.updater.UpdaterMethods
 import x7c1.wheat.ancient.context.ContextualFactory
 import x7c1.wheat.ancient.resource.ViewHolderProviderFactory
 import x7c1.wheat.macros.fragment.FragmentFactory
+import x7c1.wheat.macros.intent.ServiceCaller
+import x7c1.wheat.macros.logger.Log
 
 
 class OnSourceMenuSelected(
-  activity: FragmentActivity,
+  activity: FragmentActivity with ServiceControl,
   dialogFactory: ContextualFactory[AlertDialog.Builder],
   attachLayoutFactory: ViewHolderProviderFactory[SettingSourceAttach],
   attachRowFactory: ViewHolderProviderFactory[SettingSourceAttachRowItem]
@@ -22,10 +27,18 @@ class OnSourceMenuSelected(
   def showMenu(event: SourceMenuSelected): Unit = {
     val menu = new PopupMenu(activity, event.targetView)
     menu.getMenu.add(Menu.NONE, 123, 1, "Attached channels")
+    menu.getMenu.add(1, 234, 1, "Load now")
     menu setOnMenuItemClickListener new OnMenuItemClickListener {
       override def onMenuItemClick(item: MenuItem): Boolean = {
-        if (item.getItemId == 123){
-          createAttachSourceDialog(event) showIn activity
+        item.getItemId match {
+          case 123 =>
+            createAttachSourceDialog(event) showIn activity
+          case 234 =>
+            ServiceCaller.using[UpdaterMethods].
+              startService(activity, activity getClassOf Updater){
+                _ loadSource event.selectedSourceId
+              }
+          case x => Log error s"unknown menu-id:$x"
         }
         true
       }
