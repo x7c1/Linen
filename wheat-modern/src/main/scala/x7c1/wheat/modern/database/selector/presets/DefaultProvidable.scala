530b97877f02d18168af94bf9c236305eb6d263a
x7c1 <jmtaro+x7c1@gmail.com>
2016-05-05 23:39:11 +0900

    avoid use of primitive Long as source id

--- wheat-modern/src/main/scala/x7c1/wheat/modern/database/selector/presets/DefaultProvidable.scala
+++ wheat-modern/src/main/scala/x7c1/wheat/modern/database/selector/presets/DefaultProvidable.scala
@@ -9,4 +9,6 @@ class DefaultProvidable[I[T] <: CanIdentify[T], A]
   extends SelectorProvidable[A, DefaultSelector[I, A]](new DefaultSelector(_))
 
 class DefaultSelector[I[T] <: CanIdentify[T], A](val db: SQLiteDatabase)
-  extends Find[A] with FindBy[I, A] with CollectFrom[I, A]
+  extends Find[A]
+    with FindBy[I, A]
+    with CollectFrom[I, A]
