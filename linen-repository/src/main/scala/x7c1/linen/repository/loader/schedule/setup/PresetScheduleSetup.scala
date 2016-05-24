f7b08d6da4349ab602dd75bdfd8fefe230c4316b
x7c1 <jmtaro+x7c1@gmail.com>
2016-05-25 01:10:05 +0900

    draft to setup preset schedule

---
+++ linen-repository/src/main/scala/x7c1/linen/repository/loader/schedule/setup/PresetScheduleSetup.scala
@@ -0,0 +1,78 @@
+package x7c1.linen.repository.loader.schedule.setup
+
+import x7c1.linen.database.control.DatabaseHelper
+import x7c1.linen.database.struct.AccountIdentifiable
+
+class PresetScheduleSetup (helper: DatabaseHelper){
+
+  def findOrCreate[A: AccountIdentifiable](account: A) = {
+
+    /*
+    helper.selectorOf[PresetLoaderSchedule] findBy account matches {
+      case Right(None) => initPresetSchedule(account)
+      case Right(Some(schedule)) => Right(???)
+      case Left(e) => Left(e)
+    }
+    */
+
+  }
+  def setupPresetSchedule[A: AccountIdentifiable](account: A) = {
+    /*
+    def create() = for {
+      kind <- findKindOfAllChannels
+      scheduleId <- insertSchedule(account, kind)
+      _ <- insertTimes(scheduleId)
+    } yield {
+      scheduleId
+    }
+    findPresetSchedule(account) match {
+      case Right(Some(x)) => //nop, already created
+      case Right(None) => create()
+      case Left(e) => error!
+    }
+    */
+  }
+  /*
+  def findPresetSchedule[A: AccountIdentifiable](account: A): Option[...] = {
+    helper.selectorOf[PresetLoaderSchedule] findBy account
+  }
+  def findKindOfAllChannels = {
+    helper.selectorOf[LoaderScheduleKind] findBy ScheduleKindLabel.AllChannels matches {
+      case Right(Some(kind)) => Right(kind)
+      case Right(None) => error!
+      case Left(e) => error!
+    }
+  }
+  def insertSchedule[A: AccountIdentifiable, B: ScheduleKindIdentifiable](account: A, kind: B) = {
+    helper.writable insert LoaderScheduleParts(
+      accountId = implicitly[AccountIdentifiable[A]] toId account,
+      kindId = implicitly[ScheduleKindIdentifiable[B]] toId kind,
+      enabled = true,
+      createdAt = ...,
+    )
+  }
+  def insertTimes[A: LoaderScheduleLike](schedule: A) = {
+    val scheduleId = implicitly[LoaderScheduleLike[A]] toId schedule
+    val current = ...
+    val timePartsList = Seq(
+      ScheduleTimeParts(
+        scheduleId = scheduleId,
+        startHour = 4,
+        startMinute = 0,
+        createdAt = current
+      ),
+      ScheduleTimeParts(
+        scheduleId = scheduleId,
+        startHour = 13,
+        startMinute = 0,
+        createdAt = current
+      ),
+    )
+    timePartsList map { parts =>
+      helper.writable insert parts
+    } collect {
+      case Left(e) => Log error ...
+    }
+  }
+  */
+}
