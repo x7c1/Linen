a93a019721d561471d128e1590ac96fd9333c37e
x7c1 <jmtaro+x7c1@gmail.com>
2016-05-27 19:04:35 +0900

    remove useless comments

--- linen-repository/src/main/scala/x7c1/linen/repository/loader/schedule/selector/ScheduleSelector.scala
+++ linen-repository/src/main/scala/x7c1/linen/repository/loader/schedule/selector/ScheduleSelector.scala
@@ -13,51 +13,6 @@ class ScheduleSelector(
   protected val db: SQLiteDatabase
 ) extends ScheduleTraverser with ScheduleFinder
 
-  /*
-  private def dummySchedules(accountId: Long): Sequence[LoaderSchedule] = Sequence from Seq(
-    preset(accountId)
-  ) ++ createChannelSchedules(accountId)
-
-  private def preset(accountId: Long) = PresetLoaderSchedule(
-    scheduleId = 777,
-    accountId = accountId,
-    name = "Load channels at..",
-    enabled = true,
-    startRanges = Sequence from Seq(
-      /*
-      TimeRange(
-        ScheduleTime(Hour(22), Minute(55))
-      ),
-      TimeRange(
-        ScheduleTime(Hour(22), Minute(56))
-      ),
-      TimeRange(
-        ScheduleTime(Hour(23), Minute(18))
-      ),
-      */
-      TimeRange(
-        ScheduleTime(Hour(5), Minute(0))
-      ),
-      TimeRange(
-        ScheduleTime(Hour(13), Minute(0))
-      ),
-      TimeRange(
-        ScheduleTime(Hour(21), Minute(0))
-      )
-    )
-  )
-  private def createChannelSchedules(accountId: Long) = {
-    (0 to 20) map { n =>
-      ChannelLoaderSchedule(
-        scheduleId = 111 * n,
-        accountId = accountId,
-        name = s"Load channel : $n",
-        enabled = true
-      )
-    }
-  }
-  */
-
 trait ScheduleFinder {
   protected def db: SQLiteDatabase
 
@@ -70,5 +25,4 @@ trait ScheduleFinder {
       times = times map ScheduleTime.fromRecord
     )
   }
-
 }
