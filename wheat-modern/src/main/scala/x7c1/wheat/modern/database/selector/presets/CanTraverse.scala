e950bf0cf0d5f830534de78ac01c6598ad5b4a7b
x7c1 <jmtaro+x7c1@gmail.com>
2016-06-01 23:30:54 +0900

    add ClosableSequenceLoader to library

--- wheat-modern/src/main/scala/x7c1/wheat/modern/database/selector/presets/CanTraverse.scala
+++ wheat-modern/src/main/scala/x7c1/wheat/modern/database/selector/presets/CanTraverse.scala
@@ -1,15 +1,23 @@
 package x7c1.wheat.modern.database.selector.presets
 
+import android.database.sqlite.SQLiteDatabase
 import android.database.{Cursor, SQLException}
-import x7c1.wheat.modern.database.selector.{CanIdentify, CanSelect, CursorReadable, CursorReifiable}
+import x7c1.wheat.modern.callback.CallbackTask
+import x7c1.wheat.modern.callback.TaskProvider.async
+import x7c1.wheat.modern.database.selector.SelectorProvidable.Implicits.SelectorProvidableDatabase
+import x7c1.wheat.modern.database.selector.presets.ClosableSequenceLoader.{Done, LoaderEvent, SqlError}
+import x7c1.wheat.modern.database.selector.presets.TraverseOn.TraverseOnProvidable
+import x7c1.wheat.modern.database.selector.{CanExtract, CanIdentify, CanSelect, CursorReadable, CursorReifiable}
 import x7c1.wheat.modern.sequence.{CanMapFrom, Sequence}
 
 import scala.language.{higherKinds, reflectiveCalls}
 
-abstract class CanTraverse [I[T] <: CanIdentify[T], A]
-  extends CanSelect[I, ClosableSequence[A]]{
-
+trait CanTraverse[I[T] <: CanIdentify[T], A] extends CanExtract[I, ClosableSequence[A]]{
   override type Result[X] = Either[SQLException, X]
+}
+
+trait CanTraverseBySelect [I[T] <: CanIdentify[T], A]
+  extends CanTraverse[I, A] with CanSelect[I, ClosableSequence[A]]{
 
   override def onException(e: SQLException) = {
     Left(e)
@@ -43,5 +51,51 @@ object ClosableSequence {
         override def findAt(position: Int) = fa.findAt(position) map f
         override def length = fa.length
       }
+  }
+}
+
+class ClosableSequenceLoader[I[T] <: CanIdentify[T], A] private
+  (db: SQLiteDatabase)(implicit x1: CanTraverse[I, A], x2: TraverseOnProvidable[I, A]){
+
+  private val holder = new SequenceHolder
+
+  val sequence: Sequence[A] = holder
+
+  def startLoading[X: I](x: X): CallbackTask[LoaderEvent[A]] = async {
+    db.selectorOf[A].traverseOn(x)
+  } map {
+    case Right(xs) =>
+      holder.updateSequence(xs)
+      Done(xs)
+    case Left(e) =>
+      SqlError(e)
+  }
+  private class SequenceHolder extends Sequence[A]{
+    private var underlying: Option[ClosableSequence[A]] = None
+
+    def updateSequence(sequence: ClosableSequence[A]) = synchronized {
+      underlying foreach {_.closeCursor()}
+      underlying = Some(sequence)
+    }
+    override def length = {
+      underlying map (_.length) getOrElse 0
+    }
+    override def findAt(position: Int) = {
+      underlying flatMap (_ findAt position)
+    }
+  }
+}
+
+object ClosableSequenceLoader {
+  sealed trait LoaderEvent[A]
+  case class Done[A](sequence: ClosableSequence[A]) extends LoaderEvent[A]
+  case class SqlError[A](cause: SQLException) extends LoaderEvent[A]
+
+  def apply[I[T] <: CanIdentify[T], A]
+    (db: SQLiteDatabase)(implicit
+      x1: CanTraverse[I, A],
+      x2: TraverseOnProvidable[I, A]): ClosableSequenceLoader[I, A] = {
+
+    new ClosableSequenceLoader(db)
   }
 }
