14fe53976b029ade7fae2e27cfbc4fa8e9c65dba
x7c1 <jmtaro+x7c1@gmail.com>
2016-06-06 11:39:11 +0900

    support truncate by WritableDatabase

--- wheat-modern/src/main/scala/x7c1/wheat/modern/database/database.scala
+++ wheat-modern/src/main/scala/x7c1/wheat/modern/database/database.scala
@@ -2,6 +2,9 @@ package x7c1.wheat.modern.database
 
 import android.content.ContentValues
 
+trait HasTable[A]{
+  def tableName: String
+}
 
 trait Insertable[A] {
   def tableName: String
@@ -18,3 +21,5 @@ trait Deletable[A]{
   def tableName: String
   def where(target: A): Seq[(String, String)]
 }
+
+class AllowTruncate[A]
