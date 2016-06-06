b8e2fe1f10618c36d8fbd342ac4ca9b93cb8f11b
x7c1 <jmtaro+x7c1@gmail.com>
2016-06-06 12:06:06 +0900

    enable to mark all sources as *unread* for debug

--- linen-modern/src/main/scala/x7c1/linen/modern/init/dev/CreateRecordsDelegatee.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/dev/CreateRecordsDelegatee.scala
@@ -5,6 +5,7 @@ import android.content.{BroadcastReceiver, Context, Intent, IntentFilter}
 import android.support.v4.content.LocalBroadcastManager
 import android.widget.Toast
 import x7c1.linen.database.control.{DatabaseHelper, LinenDatabase}
+import x7c1.linen.database.struct.source_statuses
 import x7c1.linen.glue.res.layout.DevCreateRecordsLayout
 import x7c1.linen.glue.service.ServiceControl
 import x7c1.linen.glue.service.ServiceLabel.Updater
@@ -12,7 +13,9 @@ import x7c1.linen.scene.updater.UpdaterMethods
 import x7c1.linen.scene.updater.UpdaterServiceTypes.ActionTypeSample
 import x7c1.wheat.macros.intent.ServiceCaller
 import x7c1.wheat.macros.logger.Log
+import x7c1.wheat.modern.database.AllowTruncate
 import x7c1.wheat.modern.decorator.Imports._
+import x7c1.wheat.modern.formatter.ThrowableFormatter.format
 
 
 class CreateRecordsDelegatee (
@@ -65,14 +68,29 @@ class CreateRecordsDelegatee (
           _.createPresetJp()
         }
     }
+    layout.markAllAsUnread onClick { _ =>
+      implicit object truncate extends AllowTruncate[source_statuses]
+      helper.writable.truncate[source_statuses] match {
+        case Left(e) =>
+          Log error format(e){"[failed]"}
+          show(s"[failed] ${e.getMessage}")
+        case Right(rows) =>
+          Log info s"[done] deleted($rows)"
+          show(s"[done] ${source_statuses.table} rows($rows) deleted")
+      }
+    }
     layout.deleteDatabase onClick { _ =>
       activity deleteDatabase LinenDatabase.name
-      Toast.makeText(activity, "database deleted", Toast.LENGTH_SHORT).show()
+      show("database deleted")
     }
   }
   def close(): Unit = {
     Log info "[done]"
     getBroadcastManager unregisterReceiver receiver
     helper.close()
+  }
+  private def show(message: String): Unit ={
+    val toast = Toast.makeText(activity, message, Toast.LENGTH_LONG)
+    toast.show()
   }
 }
