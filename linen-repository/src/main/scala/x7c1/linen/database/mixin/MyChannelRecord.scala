35c87096baec1f96ffadef2a63e0553e81e31656
x7c1 <jmtaro+x7c1@gmail.com>
2016-06-19 18:32:02 +0900

    use Query.SqlBuilder

--- linen-repository/src/main/scala/x7c1/linen/database/mixin/MyChannelRecord.scala
+++ linen-repository/src/main/scala/x7c1/linen/database/mixin/MyChannelRecord.scala
@@ -2,6 +2,7 @@ package x7c1.linen.database.mixin
 
 import android.database.Cursor
 import x7c1.linen.database.struct.{ChannelRecord, ChannelStatusRecord, HasAccountId}
+import x7c1.wheat.macros.database.Query.SqlBuilder
 import x7c1.wheat.macros.database.{Query, TypedCursor, TypedFields}
 import x7c1.wheat.modern.database.selector.RecordReifiable
 import x7c1.wheat.modern.database.selector.presets.CanTraverseRecord
@@ -18,21 +19,19 @@ object MyChannelRecord {
   }
   implicit object traversable extends CanTraverseRecord[HasAccountId, MyChannelRecord]{
     override def queryAbout[X: HasAccountId](target: X): Query = {
-      val sql =
-        """SELECT
-          | _id,
-          | name,
-          | description,
-          | IFNULL(c2.subscribed, 0) AS subscribed,
-          | c1.created_at AS created_at
-          |FROM channels AS c1
-          | LEFT JOIN channel_statuses AS c2
-          |   ON c1._id = c2.channel_id AND c2.account_id = ?
-          |WHERE c1.account_id = ?
-          |ORDER BY c1._id DESC""".stripMargin
-
       val id = implicitly[HasAccountId[X]] toId target
-      Query(sql, Array(id.toString, id.toString))
+      sql"""
+        |SELECT
+        | _id,
+        | name,
+        | description,
+        | IFNULL(c2.subscribed, 0) AS subscribed,
+        | c1.created_at AS created_at
+        |FROM channels AS c1
+        | LEFT JOIN channel_statuses AS c2
+        |   ON c1._id = c2.channel_id AND c2.account_id = $id
+        |WHERE c1.account_id = $id
+        |ORDER BY c1._id DESC"""
     }
   }
 }
