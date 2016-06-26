7aca8998b0dd72a38ae7a2a0de274bd9c3497d02
x7c1 <jmtaro+x7c1@gmail.com>
2016-06-26 19:08:54 +0900

    eliminate unsafe use of Future

--- linen-scene/src/main/scala/x7c1/linen/scene/loader/crawling/SchedulerService.scala
+++ linen-scene/src/main/scala/x7c1/linen/scene/loader/crawling/SchedulerService.scala
@@ -4,14 +4,12 @@ import android.app.Service
 import android.content.Context
 import x7c1.linen.database.control.DatabaseHelper
 import x7c1.linen.glue.service.{ServiceControl, ServiceLabel}
-import x7c1.linen.repository.loader.crawling.Implicits._
+import x7c1.linen.repository.loader.crawling.CrawlerFate
 import x7c1.linen.repository.loader.schedule.LoaderSchedule
 import x7c1.linen.repository.loader.schedule.setup.PresetScheduleSetup
 import x7c1.wheat.macros.intent.ServiceCaller
 import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.modern.formatter.ThrowableFormatter.format
-
-import scala.concurrent.Future
 
 trait SchedulerService {
   def setupSchedule(accountId: Long): Unit
@@ -40,13 +38,13 @@ private class SchedulerServiceImpl(
   service: Service with ServiceControl,
   helper: DatabaseHelper ) extends SchedulerService {
 
-  override def setupSchedule(accountId: Long): Unit = Future {
+  override def setupSchedule(accountId: Long): Unit = CrawlerFate run {
     PresetScheduleSetup(helper) setupFor accountId
     setupAllSchedules()
-  } onFailure {
-    case e => Log error format(e){"[abort] (unexpected)"}
+  } atLeft {
+    Log error _.detail
   }
-  override def setupAllSchedules(): Unit = Future {
+  override def setupAllSchedules(): Unit = CrawlerFate run {
     helper.selectorOf[LoaderSchedule].traverseAll() match {
       case Right(schedules) =>
         // unsafe if so many schedules are added
@@ -54,24 +52,21 @@ private class SchedulerServiceImpl(
       case Left(e) =>
         Log error format(e){"[failed]"}
     }
-  } onFailure {
-    case e => Log error format(e){"[abort] (unexpected)"}
+  } atLeft {
+    Log error _.detail
   }
-
-  override def loadFromSchedule(scheduleId: Long): Unit = Future {
+  override def loadFromSchedule(scheduleId: Long): Unit = CrawlerFate run {
 
     /* setup schedule again for next call */
     scheduleNextLoader(scheduleId)
 
     SubscribedContentsLoader(service, helper) loadFromSchedule scheduleId
-
-  } onFailure {
-    case e => Log error format(e){"[abort] (unexpected)"}
+  } atLeft {
+    Log error _.detail
   }
-
-  override def scheduleNextLoader(scheduleId: Long): Unit = Future {
+  override def scheduleNextLoader(scheduleId: Long): Unit = CrawlerFate run {
     LoaderScheduler(service, helper) setupNextLoader scheduleId
-  } onFailure {
-    case e => Log error format(e){"[abort] (unexpected)"}
+  } atLeft {
+    Log error _.detail
   }
 }
--- linen-scene/src/main/scala/x7c1/linen/scene/updater/ChannelNormalizerService.scala
+++ linen-scene/src/main/scala/x7c1/linen/scene/updater/ChannelNormalizerService.scala
@@ -5,13 +5,11 @@ import android.content.Context
 import x7c1.linen.database.control.DatabaseHelper
 import x7c1.linen.glue.service.{ServiceControl, ServiceLabel}
 import x7c1.linen.repository.channel.order.ChannelOrderNormalizer
-import x7c1.linen.repository.loader.crawling.Implicits._
+import x7c1.linen.repository.loader.crawling.CrawlerFate
 import x7c1.wheat.macros.intent.ServiceCaller
 import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.macros.reify.New
 import x7c1.wheat.modern.formatter.ThrowableFormatter.format
-
-import scala.concurrent.Future
 
 trait ChannelNormalizerService {
   def normalizeRanks(accountId: Long): Unit
@@ -36,7 +34,7 @@ private class ChannelNormalizerServiceImpl(
   service: Service with ServiceControl,
   helper: DatabaseHelper ) extends ChannelNormalizerService {
 
-  override def normalizeRanks(accountId: Long): Unit = Future {
+  override def normalizeRanks(accountId: Long): Unit = CrawlerFate run {
     val updater = New[ChannelOrderNormalizer](helper.getWritableDatabase)
     val either = updater normalizeRanksOf accountId
     either match {
@@ -45,5 +43,5 @@ private class ChannelNormalizerServiceImpl(
       case Left(e) =>
         Log error format(e){"[failed]"}
     }
-  }
+  } atLeft { Log error _.detail }
 }
