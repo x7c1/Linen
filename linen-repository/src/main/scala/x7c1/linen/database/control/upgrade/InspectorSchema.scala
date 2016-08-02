15c293f4dc1d6a5b7a5a82d9a42620ef118cb6ac
x7c1 <jmtaro+x7c1@gmail.com>
2016-08-03 01:05:51 +0900

    add new tables around inspector and its actions

---
+++ linen-repository/src/main/scala/x7c1/linen/database/control/upgrade/InspectorSchema.scala
@@ -0,0 +1,29 @@
+package x7c1.linen.database.control.upgrade
+
+object InspectorSchema {
+  def init = Seq(
+    s"""
+      |CREATE TABLE IF NOT EXISTS inspector_actions (
+      |   action_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      |   loading_result INTEGER NOT NULL,
+      |   origin_title TEXT NOT NULL,
+      |   origin_url TEXT NOT NULL,
+      |   created_at INTEGER NOT NULL,
+      |   updated_at INTEGER NOT NULL
+      |)
+     """.stripMargin,
+
+    s"""
+       |CREATE TABLE IF NOT EXISTS inspector_sources (
+       |   action_id INTEGER NOT NULL,
+       |   loading_result INTEGER NOT NULL,
+       |   latent_source_url TEXT NOT NULL,
+       |   discovered_source_id INTEGER,
+       |   created_at INTEGER NOT NULL,
+       |   updated_at INTEGER NOT NULL,
+       |   FOREIGN KEY(action_id) REFERENCES inspector_actions(action_id) ON DELETE CASCADE,
+       |   FOREIGN KEY(discovered_source_id) REFERENCES sources(_id) ON DELETE SET NULL
+       |)
+     """.stripMargin
+  )
+}
