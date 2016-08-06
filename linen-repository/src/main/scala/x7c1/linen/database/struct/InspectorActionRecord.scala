2c7f2552be43f6ebd5839eedda2311a8a4254989
x7c1 <jmtaro+x7c1@gmail.com>
2016-08-07 00:54:35 +0900

    add InspectorActionParts

--- linen-repository/src/main/scala/x7c1/linen/database/struct/InspectorActionRecord.scala
+++ linen-repository/src/main/scala/x7c1/linen/database/struct/InspectorActionRecord.scala
@@ -1,41 +1,76 @@
 package x7c1.linen.database.struct
 
+import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import x7c1.linen.repository.date.Date
+import x7c1.wheat.macros.database.Query._
 import x7c1.wheat.macros.database.{TypedCursor, TypedFields}
-import x7c1.wheat.modern.database.selector.presets.{CanTraverseRecord, TraverseOn}
+import x7c1.wheat.modern.database.Insertable
+import x7c1.wheat.modern.database.selector.presets.{CanTraverseRecordByQuery, TraverseAll}
 import x7c1.wheat.modern.database.selector.{RecordReifiable, SelectorProvidable}
 
 trait InspectorActionRecord extends TypedFields {
   def action_id: Long
+
   def action_loading_status: Int --> InspectorLoadingStatus
+
   def account_id: Long
+
   def origin_title: String
+
   def origin_url: String
+
   def created_at: Int --> Date
+
   def updated_at: Int --> Date
 }
 
 object InspectorActionRecord {
   def table = "inspector_actions"
+
   def column = TypedFields.expose[InspectorActionRecord]
 
   implicit object providable
     extends SelectorProvidable[InspectorActionRecord, Selector]
 
-  implicit object reifiable extends RecordReifiable[InspectorActionRecord]{
+  implicit object reifiable extends RecordReifiable[InspectorActionRecord] {
     override def reify(cursor: Cursor) = TypedCursor[InspectorActionRecord](cursor)
   }
-  implicit object traverseOn extends CanTraverseRecord[HasAccountId, InspectorActionRecord]{
-    override def queryAbout[X: HasAccountId](target: X) = {
-      ???
-    }
-  }
+
+  implicit object traverseAll extends CanTraverseRecordByQuery[InspectorActionRecord](
+    sql"SELECT * FROM inspector_actions"
+  )
+
   class Selector(protected val db: SQLiteDatabase)
-    extends TraverseOn[HasAccountId, InspectorActionRecord]
+    extends TraverseAll[InspectorActionRecord]
+
 }
 
+case class InspectorActionParts(
+  loadingStatus: InspectorLoadingStatus,
+  accountId: Long,
+  originTitle: String,
+  originUrl: String,
+  createdAt: Date,
+  updatedAt: Date
+)
 
+object InspectorActionParts {
+  import InspectorActionRecord.column
 
-case class InspectorActionParts()
+  implicit object insertable extends Insertable[InspectorActionParts] {
+    override def tableName: String = InspectorActionRecord.table
+
+    override def toContentValues(target: InspectorActionParts): ContentValues =
+      TypedFields.toContentValues(
+        column.action_loading_status -> target.loadingStatus,
+        column.account_id -> target.accountId,
+        column.origin_title -> target.originTitle,
+        column.origin_url -> target.originUrl,
+        column.created_at -> target.createdAt,
+        column.updated_at -> target.updatedAt
+      )
+  }
+
+}
