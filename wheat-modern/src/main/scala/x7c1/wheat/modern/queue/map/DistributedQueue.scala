5888818d8835b391c1b1732ef98beca21ee4a37c
x7c1 <jmtaro+x7c1@gmail.com>
2016-08-28 01:41:17 +0900

    use actual type not alias

--- wheat-modern/src/main/scala/x7c1/wheat/modern/queue/map/DistributedQueue.scala
+++ wheat-modern/src/main/scala/x7c1/wheat/modern/queue/map/DistributedQueue.scala
@@ -4,7 +4,6 @@ import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.modern.fate.FateProvider.{ErrorLike, HasContext}
 import x7c1.wheat.modern.fate.FutureFate
 import x7c1.wheat.modern.fate.FutureFate.HasTimer
-import x7c1.wheat.modern.features.HasInstance
 import x7c1.wheat.modern.kinds.Fate
 import x7c1.wheat.modern.queue.map.DistributedQueue.{CanDump, ThrowableLike}
 
@@ -17,8 +16,12 @@ trait DistributedQueue[C, L, X, Y] {
 }
 
 object DistributedQueue {
-  type ThrowableLike[X] = HasInstance[X => Throwable]
-  type CanDump[X] = HasInstance[X => String]
+  trait ThrowableLike[X] {
+    def throwableFrom(x: X): Throwable
+  }
+  trait CanDump[X] {
+    def dump(x: X): String
+  }
 }
 
 private class DistributedQueueImpl[
@@ -55,12 +58,12 @@ private class DistributedQueueImpl[
     map remove x match {
       case Some(promise) => either match {
         case Left(e) =>
-          promise failure implicitly[ThrowableLike[L]].instance(e)
+          promise failure implicitly[ThrowableLike[L]].throwableFrom(e)
         case Right(y) =>
           promise success y
       }
       case None =>
-        val dumped = implicitly[CanDump[X]].instance(x)
+        val dumped = implicitly[CanDump[X]] dump x
         Log warn s"not enqueued or already dequeued [$dumped]"
     }
   }
