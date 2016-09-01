3905add3546b80707a9419d8ce29be7a30a3c363
x7c1 <jmtaro+x7c1@gmail.com>
2016-09-02 01:34:10 +0900

    add UrlTraverser

---
+++ linen-repository/src/main/scala/x7c1/linen/repository/loader/queueing/UrlTraverser.scala
@@ -0,0 +1,31 @@
+package x7c1.linen.repository.loader.queueing
+
+import x7c1.linen.repository.loader.crawling.CrawlerContext
+import x7c1.wheat.modern.kinds.Fate
+import x7c1.wheat.modern.queue.map.{TrackableQueue, ValueQueue}
+
+trait UrlTraverser[A <: UrlEnclosure, B <: UrlTraverserOutput] {
+  def startLoading(url: A): Fate[CrawlerContext, UrlTraverserError, B]
+}
+
+private class UrlTraverserImpl[A <: UrlEnclosure, B <: UrlTraverserOutput](
+  callee: A => Either[UrlTraverserError, B]
+) extends UrlTraverser[A, B] {
+
+  private val queue = TrackableQueue[CrawlerContext, UrlTraverserError, A, B](
+    valueQueue = UrlEnclosureQueue(),
+    callee = callee
+  )
+
+  override def startLoading(url: A) = {
+    queue.enqueue(url)
+  }
+}
+
+object UrlEnclosureQueue {
+  def apply[A <: UrlEnclosure](): ValueQueue[A] = {
+    ValueQueue.toDistribute(getGroupKey = _.raw.getHost)
+  }
+}
+
+trait UrlTraverserOutput
