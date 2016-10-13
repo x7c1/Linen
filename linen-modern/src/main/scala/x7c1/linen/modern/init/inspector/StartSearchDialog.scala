c9f0d308fdd4cb60e74af9bd90ae489982290058
x7c1 <jmtaro+x7c1@gmail.com>
2016-10-13 22:21:29 +0900

    remove duplicates

--- linen-modern/src/main/scala/x7c1/linen/modern/init/inspector/StartSearchDialog.scala
+++ linen-modern/src/main/scala/x7c1/linen/modern/init/inspector/StartSearchDialog.scala
@@ -17,6 +17,7 @@ import x7c1.linen.repository.inspector.ActionPageUrlError.{EmptyUrl, InvalidForm
 import x7c1.linen.scene.inspector.InspectorService
 import x7c1.wheat.ancient.context.ContextualFactory
 import x7c1.wheat.ancient.resource.ViewHolderProviderFactory
+import x7c1.wheat.lore.dialog.DelayedDialog
 import x7c1.wheat.macros.fragment.TypedFragment
 import x7c1.wheat.macros.logger.Log
 import x7c1.wheat.modern.decorator.Imports._
@@ -33,7 +34,10 @@ object StartSearchDialog {
 
 }
 
-class StartSearchDialog extends DialogFragment with TypedFragment[Arguments] {
+class StartSearchDialog extends DialogFragment
+  with DelayedDialog
+  with TypedFragment[Arguments] {
+
   private lazy val args = getTypedArguments
 
   private lazy val keyboard = {
@@ -45,18 +49,21 @@ class StartSearchDialog extends DialogFragment with TypedFragment[Arguments] {
   }
 
   override def onCreateDialog(savedInstanceState: Bundle): Dialog = {
-    internalDialog
+    args.dialogFactory.createAlertDialog(
+      title = "Search sources",
+      positiveText = "Start",
+      negativeText = "Cancel",
+      layoutView = layout.itemView
+    )
   }
 
   override def onStart(): Unit = {
     super.onStart()
 
-    getDialog match {
-      case dialog: AlertDialog =>
-        dialog.positiveButton foreach (_ onClick onClickPositive)
-        dialog.negativeButton foreach (_ onClick onClickNegative)
-    }
-
+    initializeButtons(
+      positive = onClickPositive,
+      negative = onClickNegative
+    )
   }
 
   private def onClickPositive(button: Button) = {
@@ -95,19 +102,5 @@ class StartSearchDialog extends DialogFragment with TypedFragment[Arguments] {
   private lazy val layout = {
     val factory = args.inputLayoutFactory create getActivity
     factory inflateOn null // todo: eradicate
-  }
-  private lazy val internalDialog = {
-    val nop = new OnClickListener {
-      override def onClick(dialog: DialogInterface, which: Int): Unit = {
-        Log info s"[init]"
-      }
-    }
-    val builder = args.dialogFactory.newInstance(getActivity).
-      setTitle("Search sources").
-      setPositiveButton("Start", nop).
-      setNegativeButton("Cancel", nop)
-
-    builder setView layout.itemView
-    builder.create()
   }
 }
