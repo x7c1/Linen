569da0554dd804c69b016ba78e7ae5e5ea5663c0
x7c1 <jmtaro+x7c1@gmail.com>
2017-01-29 20:34:56 +0900

    fix macro error of LocalBroadcastListener
    that says "Trying to access the this of another class"

--- wheat-macros/src/main/scala/x7c1/wheat/macros/intent/LocalBroadcastListener.scala
+++ wheat-macros/src/main/scala/x7c1/wheat/macros/intent/LocalBroadcastListener.scala
@@ -96,8 +96,8 @@ private trait LocalBroadcastListenerFactory  extends TreeContext {
     val eventTree = IntentDecoder(this.context)(intent) decodeIntent (instanceType, prefix)
 
     q"""
-      new ${typeOf[BroadcastReceiver]}{
-        override def onReceive($context: ${typeOf[Context]}, $intent: ${typeOf[Intent]}) = {
+      new ${typeOf[BaseBroadcastReceiver]}(
+        ($context: ${typeOf[Context]}, $intent: ${typeOf[Intent]}) => {
           try {
             val $event = $eventTree
             val $f = $blockTree
@@ -111,10 +111,14 @@ private trait LocalBroadcastListenerFactory  extends TreeContext {
               ${typeOf[Log].companion}.e(${enclosing.fullName}, $message)
           }
         }
-      }
+      )
     """
   }
   def createFilter = {
     q"""new ${typeOf[IntentFilter]}(${instanceType.typeSymbol.fullName})"""
   }
+}
+
+private class BaseBroadcastReceiver(f: (Context, Intent) => Unit) extends BroadcastReceiver {
+  override def onReceive(context: Context, intent: Intent): Unit = f(context, intent)
 }
